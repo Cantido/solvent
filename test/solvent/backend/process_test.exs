0f5ceae0f91de1841ce7f8b1480660053d985787
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-05-13 14:55:55 -0600

    Test type matching for process backends;

--- test/solvent/backend/process_test.exs
+++ test/solvent/backend/process_test.exs
@@ -15,5 +15,22 @@ defmodule Solvent.Backend.ProcessTest do
 
     assert_receive ^test_ref
   end
+
+  test "only notifies listenes with an equal type" do
+    {:ok, bus} = Solvent.Backend.Process.new()
+    test_pid = self()
+
+    {:ok, bus} = Solvent.subscribe(bus, "ID 1", "event.published", fn _data ->
+      send test_pid, :expected_handler
+    end)
+    {:ok, bus} = Solvent.subscribe(bus, "ID 2", "other.event.published", fn _data ->
+      send test_pid, :other_handler
+    end)
+
+    {:ok, _} = Solvent.publish(bus, "event.published", :event_data)
+
+    assert_receive :expected_handler
+    refute_receive :other_handler
+  end
 end
 
