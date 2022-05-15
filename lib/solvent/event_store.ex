9f648cdb300b4335ca0210cf6d6531cf47270f1b
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-05-15 11:52:32 -0600

    widdle wefactor

--- lib/solvent/event_store.ex
+++ lib/solvent/event_store.ex
@@ -3,11 +3,13 @@ defmodule Solvent.EventStore do
   ETS-based storage for `Solvent.Event` objects.
   """
 
+  @table_name :solvent_event_store
+
   @doc """
   Fetch an event by ID.
   """
   def fetch(id) do
-    case :ets.lookup(:solvent_event_store, id) do
+    case :ets.lookup(@table_name, id) do
       [{_id, event}] -> {:ok, event}
       _ -> :error
     end
@@ -19,7 +21,7 @@ defmodule Solvent.EventStore do
   This does not activate any subscribers, use `Solvent.publish/2` for that.
   """
   def insert(event) do
-    true = :ets.insert(:solvent_event_store, {event.id, event})
+    true = :ets.insert(@table_name, {event.id, event})
     :ok
   end
 
@@ -27,7 +29,7 @@ defmodule Solvent.EventStore do
   Remove an event from storage.
   """
   def delete(event_id) do
-    true = :ets.delete(:solvent_event_store, event_id)
+    true = :ets.delete(@table_name, event_id)
     :ok
   end
 
@@ -35,7 +37,7 @@ defmodule Solvent.EventStore do
   Delete all events from the event store.
   """
   def delete_all do
-    true = :ets.delete_all_objects(:solvent_event_store)
+    true = :ets.delete_all_objects(@table_name)
     :ok
   end
 end
