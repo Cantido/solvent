b0f21d9cba5bf6d85c30ee87a215bf891722c384
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-05-16 10:56:42 -0600

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -4,15 +4,17 @@ defmodule Solvent.MixProject do
   def project do
     [
       app: :solvent,
+      descritpion: "A fast, in-memory event bus",
       version: "0.1.0",
       elixir: "~> 1.13",
+      source_url: "https://github.com/Cantido/solvent",
+      package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -20,15 +22,23 @@ defmodule Solvent.MixProject do
     ]
   end
 
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/Cantido/solvent",
+        "Sponsor" => "https://liberapay.org/rosa"
+      }
+    ]
+  end
+
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:elixir_uuid, "~> 1.2"},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:telemetry, "~> 1.0"}
     ]
   end
